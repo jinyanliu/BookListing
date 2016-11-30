e2619360f2d1aa3ff20be13436039690ab92cc47
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-11-30 17:36:13 +0100

    Add the function when touching outside of the editText, the keyboard disappear from the screen.

--- app/src/main/java/se/sugarest/jane/booklisting/MainActivity.java
+++ app/src/main/java/se/sugarest/jane/booklisting/MainActivity.java
@@ -1,7 +1,11 @@
 package se.sugarest.jane.booklisting;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.search_item);
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
     }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 }
